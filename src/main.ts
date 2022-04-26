57a7119fcd786d131df4b4dc60a3c416ca48b674
Vladimir Barkasov <vbarkasov4dev@gmail.com>
2022-04-26 20:39:11 +0700

    Fix icons

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,25 @@ import { VueReCaptcha } from 'vue-recaptcha-v3'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faGooglePlay } from '@fortawesome/free-brands-svg-icons'
+library.add(faGithub, faGooglePlay, faExternalLinkAlt)
 
 const gRecaptchaSitekey =  import.meta.env.VITE_RECAPTCHA_SITEKEY
 console.log('gRecaptchaSitekey: ' + import.meta.env.VITE_RECAPTCHA_SITEKEY)
 
 loadFonts()
 
-createApp(App)
-    .use(createPinia())
-    .use(router)
-    .use(vuetify)
-    .use(VueReCaptcha, { siteKey: gRecaptchaSitekey })
-    .mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+app.use(VueReCaptcha, { siteKey: gRecaptchaSitekey })
+app.component('fa-icon', FontAwesomeIcon)
+app.mount('#app')
 
 
 
