1aed9df81a1158764e6cdc394ca19a900662d062
Vladimir Barkasov <vbarkasov4dev@gmail.com>
2022-04-27 14:42:33 +0700

    Remove prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,21 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
   ],
-  "rules": {
-    quotes: ["error", "single"]
+  rules: {
+    quotes: ['error', 'single'],
+    indent: [2, 2, { SwitchCase: 1}]
   },
-  "parserOptions": {
-    "parser": "@babel/eslint-parser"
+  parserOptions: {
+    parser: '@babel/eslint-parser'
   },
-  "env": {
-    "node": true,
-    "vue/setup-compiler-macros": true
+  env: {
+    node: true,
+    'vue/setup-compiler-macros': true
   }
 }
